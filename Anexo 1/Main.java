09d2dd9816bbe7454cdd24e25179782fb53ec4c5
RobertQH123 <robertqhjaki@gmail.com>
2024-05-03 20:57:09 -0500

    Agrega lógica de simulación de compras en el main con dos cajeras y dos clientes

--- Anexo 1/Main.java
+++ Anexo 1/Main.java
@@ -1,7 +1,21 @@
 
 public class Main {
-	public static void main(String[] args) {	
-	
-	}
+    public static void main(String[] args) {
+        
+        cajera cajera1 = new cajera("Cajera 1");
+        cajera cajera2 = new cajera("Cajera 2");
 
+    
+        Cliente cliente1 = new Cliente("Cliente 1", new int[]{5, 2, 3, 4});
+        Cliente cliente2 = new Cliente("Cliente 2", new int[]{2, 1, 5, 3});
+
+        
+        long tiempoInicial = System.currentTimeMillis();
+
+        
+        cajera1.procesarCompra(cliente1, tiempoInicial);
+
+        cajera2.procesarCompra(cliente2, tiempoInicial);
+    }
 }
+
