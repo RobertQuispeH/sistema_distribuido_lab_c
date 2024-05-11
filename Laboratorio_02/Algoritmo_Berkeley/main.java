d28f6f4b6af0bcb9d082776bdd5225bc4b535672
KevinAMamaniC <64039060+KevinAMamaniC2020@users.noreply.github.com>
2024-05-10 23:32:53 -0500

    Prueba básica del algoritmo Berkeley

--- Laboratorio_02/Algoritmo_Berkeley/main.java
+++ Laboratorio_02/Algoritmo_Berkeley/main.java
@@ -1,5 +1,21 @@
 public class main {
 	public static void main(String[] args) {
+
+		//Creamos dos computadoras,condiferente tiempo
+		Maquina comp1 = new Maquina("Comp1", 10);
+       	 	Maquina comp2 = new Maquina("Comp2", 20);
+
+		//Llamamos al servidor
+		 Servidor servidorTiempo = new Servidor();
+		//Incluimos al servidor las computadoras creadas
+		 servidorTiempo.agregarComputadora(comp1);
+       		 servidorTiempo.agregarComputadora(comp2);
+
+		 // Llamamos al metodo sincronizar para ejecutar Berkeley
+	        servidorTiempo.sincronizarTiempo();
+	
+	        // Imprimimos el tiempo sincronizado.
+	        servidorTiempo.imprimirTiempos();
     
 	}
 }
