a0763f07e25ef0cb4a38bd77f770b95ff9a4ada6
Omar <oromero@unsa.edu.pe>
2024-05-14 21:37:59 -0500

    Instancia para cada cliente

--- Laboratorio_03/Server.java
+++ Laboratorio_03/Server.java
@@ -164,4 +164,28 @@ public class Server {
 		}
 		broadcast(notif + disconnectedClient + " has left the chat room." + notif);
 	}
+
+	public static void main(String[] args) {
+		// start server on port 1500 unless a PortNumber is specified
+		int portNumber = 1500;
+		switch (args.length) {
+		case 1:
+			try {
+				portNumber = Integer.parseInt(args[0]);
+			} catch (Exception e) {
+				System.out.println("Invalid port number.");
+				System.out.println("Usage is: > java Server [portNumber]");
+				return;
+			}
+		case 0:
+			break;
+		default:
+			System.out.println("Usage is: > java Server [portNumber]");
+			return;
+		}
+		// create a server object and start it
+		Server server = new Server(portNumber);
+		server.start();
+	}
+
 }
