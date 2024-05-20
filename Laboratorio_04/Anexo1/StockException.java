ace936b32da802c7684c1909acbdd3688e03709c
KevinAMamaniC <64039060+KevinAMamaniC2020@users.noreply.github.com>
2024-05-20 18:44:10 -0500

    comentando Stock Exception

--- Laboratorio_04/Anexo1/StockException.java
+++ Laboratorio_04/Anexo1/StockException.java
@@ -1,5 +1,5 @@
 package Anexo1;
-
+// Usadopara manejar errores específicos relacionados con el stock.
 public class StockException extends Exception{
 	public StockException(String msg) {
 		super(msg);
