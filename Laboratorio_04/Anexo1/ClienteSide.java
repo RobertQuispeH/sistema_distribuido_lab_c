ff1a46374cd8f5e3a0c096a8921f6e94c20d92aa
KevinAMamaniC <64039060+KevinAMamaniC2020@users.noreply.github.com>
2024-05-20 20:14:28 -0500

    If de la opcion 1

--- Laboratorio_04/Anexo1/ClienteSide.java
+++ Laboratorio_04/Anexo1/ClienteSide.java
@@ -12,7 +12,20 @@ public class ClienteSide {
         StockInterface pharmacy = (StockInterface) Naming.lookup("PHARMACY"); // Busca el objeto remoto "PHARMACY"
         System.out.println("Ingrese la opción\n1: Listar productos\n2: Comprar Producto\n"); // Muestra opciones al usuario
 
-        int selection = sc.nextInt(); // Lee la opción seleccionada por el usuario
+        int selection = sc.nextInt(); // Ingresa y lee la opcion ingresada por consola
+         if (selection == 1) {
+            // Si es 1lista los medicamentos que estan el HashMap
+            HashMap<String, MedicineInterface> products = pharmacy.getStockProducts(); // Los obtiene del hashmap
+            for (String key : products.keySet()) { // Pero debemos iterar sobre los procutos
+                MedicineInterface medicine = products.get(key);
+                System.out.println(medicine.print()); // Una vez recorrido todo, imprime la informacion
+                System.out.println("*--------------*");
+            }
+        } 
+        } else {
+            System.out.println("Seleccione una opción válida"); // Mensaje en caso de opción inválida
+        }
+        sc.close(); // Cierra el ingreso por consola
        
     }
 }
