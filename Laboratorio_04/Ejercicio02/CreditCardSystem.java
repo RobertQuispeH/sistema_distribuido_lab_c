c951ed0d4c251a184059d624e42efb406166d69f
Elvis-321 <83074216+Elvis-321@users.noreply.github.com>
2024-05-21 02:08:37 -0500

    Update CreditCardSystem.java
    Se ha comentado los metodos para tener mejor entendimiento de estos

--- Laboratorio_04/Ejercicio02/CreditCardSystem.java
+++ Laboratorio_04/Ejercicio02/CreditCardSystem.java
@@ -2,10 +2,12 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface CreditCardSystem extends Remote {
-    
+    //verifica si la tarjeta ingresada como parametro es valida
     boolean validateCard(String cardNumber) throws RemoteException;
 
+    //devuelve el balance o la cantidad con la que se cuenta en la tarjeta
     double getBalance(String cardNumber) throws RemoteException;
 
+    //devuelve true en caso el cargo se haya realizado correctamente
     boolean charge(String cardNumber, double amount) throws RemoteException;
 }
