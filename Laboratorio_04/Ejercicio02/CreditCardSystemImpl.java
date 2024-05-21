ba0f3398240a662a3ac1565849de16d660fa29c9
Elvis-321 <83074216+Elvis-321@users.noreply.github.com>
2024-05-21 02:12:48 -0500

    Update CreditCardSystemImpl.java
    Se ha añadido mas ejemplos de tarjetas al hash map que haria como la bd

--- Laboratorio_04/Ejercicio02/CreditCardSystemImpl.java
+++ Laboratorio_04/Ejercicio02/CreditCardSystemImpl.java
@@ -11,6 +11,13 @@ public class CreditCardSystemImpl extends UnicastRemoteObject implements CreditC
         cardDatabase = new HashMap<>();
         cardDatabase.put("1234-5678-9101-1121", 1000.0);
         cardDatabase.put("4321-8765-1011-1211", 500.0);
+        cardDatabase.put("4321-8765-1011-1211", 500.0);
+        cardDatabase.put("5678-1234-1111-2222", 750.0);
+        cardDatabase.put("8765-4321-3333-4444", 300.0);
+        cardDatabase.put("1111-2222-3333-4444", 1200.0);
+        cardDatabase.put("2222-3333-4444-5555", 450.0);
+        cardDatabase.put("3333-4444-5555-6666", 800.0);
+        cardDatabase.put("4444-5555-6666-7777", 600.0);
     }
 
     public boolean validateCard(String cardNumber) throws RemoteException {
