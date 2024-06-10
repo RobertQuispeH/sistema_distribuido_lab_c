71bbfcd34a53dd145d2f3d20f84f5b14c283ee5a
KevinAMamaniC <64039060+KevinAMamaniC2020@users.noreply.github.com>
2024-06-10 00:18:26 -0500

    Creando el archivo implementación

---
+++ Laboratorio_07/WSSOAP/src/java/es/rosamarfil/soap/SOAPImpl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.rosamarfil.soap;
+
+import java.util.List;
+import javax.jws.WebService;
+import es.rosamarfil.model.User;
+
+@WebService(endpointInterface = "es.rosamarfil.soap.SOAPI")
+public class SOAPImpl implements SOAPI {
+  
+}
