f8578dffae21e047652a7e163de90fc43f0389f8
KevinAMamaniC <64039060+KevinAMamaniC2020@users.noreply.github.com>
2024-06-10 00:17:04 -0500

    creando la clase SOAPI

---
+++ Laboratorio_07/WSSOAP/src/java/es/rosamarfil/soap/SOAPI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.rosamarfil.soap;
+ 
+import java.util.List;
+ 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+ 
+import es.rosamarfil.model.User;
+ 
+@WebService
+public interface SOAPI {
+     
+    
+ 
+}
