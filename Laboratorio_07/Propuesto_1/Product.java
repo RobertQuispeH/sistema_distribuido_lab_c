26ae4475c0c695898d7e1bf15ba088734065764c
Elvis-321 <83074216+Elvis-321@users.noreply.github.com>
2024-06-11 06:45:08 -0500

    Se ha agregado mas productos

--- Laboratorio_07/Propuesto_1/Product.java
+++ Laboratorio_07/Propuesto_1/Product.java
@@ -1,4 +1,4 @@
-package LabSD;
+package labsd;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -7,42 +7,54 @@ import java.util.List;
 
 public class Product implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public static List<Product> products = new ArrayList<>(Arrays.asList(
-			new Product("Lapiceros", 0.80),
-			new Product("Cuadernos", 3.40),
-			new Product("Hoja Bond", 10.50)));
+			new Product("Lapiceros", 0.80, 100),
+			new Product("Cuadernos", 3.40, 50),
+			new Product("Hoja Bond", 10.50, 200),
+			new Product("Gomas de Borrar", 1.20, 150),
+			new Product("Sacapuntas", 0.50, 80),
+			new Product("Marcadores", 2.50, 60),
+			new Product("Tijeras", 3.00, 40),
+			new Product("Reglas", 1.50, 70),
+			new Product("Calculadoras", 15.00, 30),
+			new Product("Compás", 4.00, 50)
+	));
 	
 	public String name;
 	public double price;
+	public int stock;
+
 	public Product() {
 		super();
 	}
 	
-	public Product(String name, double price) {
+	public Product(String name, double price, int stock) {
 		super();
 		this.name = name;
 		this.price = price;
+		this.stock = stock;
 	}
 	
 	public void setName(String name) {
 		this.name = name;
 	}
 	
-	public void setPrice(double precio) {
-		this.price = precio;
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
 	}
 	
 	public static List<Product> getProducts(){
 		return products;
 	}
 	
+	@Override
 	public String toString() {
-		return "Producto [name = " + name + ", precio = " + price + "]";
+		return "Product [name=" + name + ", price=" + price + ", stock=" + stock + "]";
 	}
-	
 }
