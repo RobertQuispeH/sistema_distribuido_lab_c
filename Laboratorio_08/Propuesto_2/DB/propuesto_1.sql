a0aa7115f133bdb0ad8b9a1a087f21d7b09bcb5b
RobertQuispeH <rquispehuach@unsa.edu.pe>
2024-06-13 05:51:16 -0500

    modificar el .sql para crear la base de datos si esque no exite, y ademas crear un docker-composer inisializado con nuestro sql

--- Laboratorio_08/Propuesto_2/DB/propuesto_1.sql
+++ Laboratorio_08/Propuesto_2/DB/propuesto_1.sql
@@ -7,6 +7,10 @@
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
+CREATE DATABASE IF NOT EXISTS `propuesto_1`;
+USE `propuesto_1`;
+
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
